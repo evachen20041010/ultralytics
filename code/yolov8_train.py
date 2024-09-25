1f5df2517dc3943b2cd64c572468a69ded3292ce
EvaChen <a0937510149@gmail.com>
2024-09-26 02:25:04 +0800

    訓練模型，恢復成 9/18 程式碼，並移除無用的變數

--- code/yolov8_train.py
+++ code/yolov8_train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('models/yolov8x.pt')
+model = YOLO('models/v9.pt')
 
 # 用程式碼執行時，workers 要改成0
-model.train(data = 'coco8_4.yaml', workers = 0, epochs = 50, batch = 16)
+model.train(data = 'coco8_5.yaml', workers = 0, epochs = 50, batch = 16)
