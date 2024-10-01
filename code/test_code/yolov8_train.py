578a22f550e612ad53ad163b7fe146d00ff5ea3b
EvaChen <a0937510149@gmail.com>
2024-10-01 15:52:05 +0800

    20240930 訓練模型 v9

--- code/test_code/yolov8_train.py
+++ code/test_code/yolov8_train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('models/v9.pt')
+model = YOLO('models/yolov8x.pt')
 
 # 用程式碼執行時，workers 要改成0
-model.train(data = 'coco8_5.yaml', workers = 0, epochs = 50, batch = 16)
+model.train(data = 'yaml/coco8_6.yaml', workers = 0, epochs = 50, batch = 16)
